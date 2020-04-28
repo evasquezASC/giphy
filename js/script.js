e45a615d4297929be96ceb4f78e29cb7b1f5a206
evasquezASC <ernievazk7979@gmail.com>
2020-04-28 21:59:25 +0000

    added fetch response

--- js/script.js
+++ js/script.js
@@ -4,7 +4,20 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
+    let choice = $("#search-term").val();
+    let request = "https://api.giphy.com/v1/gifs/search?q=" 
+    + choice +
+    "&rating=pg&api_key=dc6zaTOxFJmzC";
+  fetch(request)
+    .then(function(response) {
+      console.log(response)  
+      return response.json();
+    })
+    .then(function(data) {
+    // debugger;   
+        let pic_url = data.data[0].images.preview_gif.url;
+     $('#result').append("<img src=" + pic_url + ">");
+    });
   
   
 });
